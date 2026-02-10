78e67e3e2e1dcb6c61f09311696113c1906be3a9
Rich Tebb <rich@tebb.io>
2026-02-10 22:03:28 +0000

    Add skeleton Vite React app

--- src/HookNorton.AppHost/AppHost.cs
+++ src/HookNorton.AppHost/AppHost.cs
@@ -1,6 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.HookNorton>("api")
-    .WithHttpHealthCheck("/$$/health");
+    .WithHttpHealthCheck("/$$/health")
+    .WithExternalHttpEndpoints();
+
+builder.AddViteApp("webfrontend", "../frontend")
+    .WithReference(api)
+    .WaitFor(api);
 
 builder.Build().Run();
